7f133e7112caff26bc3702154672487510384192
sajjal <nairsajjal@gmail.com>
2019-06-28 17:06:23 +0530

    Graph-i-ql implemented

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 const app= express();
 
-app.use(bodyParser.json());
 
-app.get('/', (req,res)=>{
-    res.send('yoo');
-})
+app.use(bodyParser.json());
+app.use('/graphql', graphqlHttp({
+    schema :buildSchema(`
+
+        type RootQuery{
+            events: [String!]!
+
+        }
+        type RootMutation{
+            crateEvent(name: String): String
+
+        }
+        schema{
+
+            query: RootQuery
+            mutation: RootMutation
+
+        }
+    `),
+//resolvers
+    rootValue: {events:() => {
+        return ['cooking', 'sailing', 'coding']
+    },
+    createEvent: (args) =>{
+        const eventName = args.name;
+        return eventName;
+    } },
+    graphiql: true
+}))
+
+
+
 app.listen(3000);
