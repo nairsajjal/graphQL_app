216b6c4d45fcae597d07adf2ddd848bb67b86097
sajjal <nairsajjal@gmail.com>
2019-07-01 10:37:36 +0530

    query enablement

--- app.js
+++ app.js
@@ -3,18 +3,32 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const app= express();
-
+const events = [];
 
 app.use(bodyParser.json());
 app.use('/graphql', graphqlHttp({
     schema :buildSchema(`
 
+
+        type Event {
+            _id: ID! 
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
         type RootQuery{
-            events: [String!]!
+            events: [Event!]!
 
         }
+        input EventInput{
+            title:String!
+            description:String!
+            price: Float!
+            date: String!
+        }
         type RootMutation{
-            createEvent(name: String): String
+            createEvent(eventInput :EventInput): Event
 
         }
         schema{
@@ -26,11 +40,19 @@ app.use('/graphql', graphqlHttp({
     `),
 //resolvers
     rootValue: {events:() => {
-        return ['cooking', 'sailing', 'coding']
+       return events;
     },
     createEvent: (args) =>{
-        const eventName = args.name;
-        return eventName;
+        const event = {
+            _id: Math.random().toString(),
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date : args.eventInput.date
+
+        };
+        events.push(event);
+        return event;
     } },
     graphiql: true
 }))
